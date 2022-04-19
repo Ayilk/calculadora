cca6f5fcbf36c615e7305ae3071149b9939b7d72
Ayilk <srangel.h92@gmail.com>
2022-04-18 23:38:24 -0500

    First Commit

---
+++ src/componentes/Boton.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../estilos/Boton.css';
+
+export default function Boton(props){
+
+    const esOperador = valor => {
+        return isNaN(valor) && (valor !== '.') && (valor !== '=');
+    }
+    return (
+        <div className={`boton-contenedor ${esOperador(props.children) ? 'operador' : null}`.trimEnd()} >
+            {props.children}
+        </div>
+    )
+}
